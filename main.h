8e567f25dd794eed1f507ba9a22903ad6d483d25
nikhil singh <nikhilsingh526452@gmail.com>
2020-01-30 18:40:09 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,15 @@
+#include <conio>
+#include <stdio>
+#include <stdlib>
+#include <iostream>
+#define C cout<<"congrats you solved it!!";
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+void play();
+void setarray(int *x);
+void draw(int *x);
+int checksortarray(int *x);
+void swap(int *x,int *b);
